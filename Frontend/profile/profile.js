f937351ae36e383ef6c0d141a135591be82399a1
swariggg <arianaremache2007@gmail.com>
2026-06-15 01:17:44 -0500

    integracion de jquery

--- Frontend/profile/profile.js
+++ Frontend/profile/profile.js
@@ -1,45 +1,54 @@
 const usuarioId = localStorage.getItem("usuario_id");
 const usernameLogueado = localStorage.getItem("username");
 
-document.addEventListener("DOMContentLoaded", () => {
-    if (!localStorage.getItem("usuario_autenticado") === "true" || !usuarioId) {
+$(async function() {
+
+    if (localStorage.getItem("usuario_autenticado") !== "true" || !usuarioId) {
         window.location.href = "../login/login.html";
         return;
     }
+
+    $("#profile-username").text(`@${usernameLogueado}`);
     
-    document.getElementById("profile-username").textContent = `@${usernameLogueado}`;
-    cargarMisPines();
+    await cargarMisPines();
 });
 
 async function cargarMisPines() {
-    const grid = document.getElementById("user-pin-grid");
-    grid.innerHTML = "";
+    const $grid = $("#user-pin-grid");
+    $grid.empty(); 
 
     try {
-        const res = await fetch(`http://127.0.0.1:8000/api/v1/pins/user/${usuarioId}`);
-        const pines = await res.json();
+//peticion mediante jquery
+        const pines = await $.ajax({
+            url: `http://127.0.0.1:8000/api/v1/pins/user/${usuarioId}`,
+            method: 'GET',
+            dataType: 'json'
+        });
 
-        document.getElementById("pin-count").textContent = pines.length;
+        $("#pin-count").text(pines.length);
 
         if (pines.length === 0) {
-            grid.innerHTML = `<p style="color: var(--text-muted); text-align: center; width: 100%;">Aún no has publicado ninguna idea en Fyntasy.</p>`;
+            $grid.html(`<p style="color: var(--text-muted); text-align: center; width: 100%;">Aún no has publicado ninguna idea en Fyntasy.</p>`);
             return;
         }
-
-        pines.forEach(pin => {
-            const card = document.createElement("div");
-            card.className = "pin-card";
-            
+//se agrega interacciòn con jquery
+        $.each(pines, function(index, pin) {
             const rutaImagen = pin.source.startsWith("http") ? pin.source : `http://127.0.0.1:8000/${pin.source}`;
 
-            card.innerHTML = `
+            const $card = $("<div>", { class: "pin-card" });
+            
+            $card.html(`
                 <img src="${rutaImagen}" alt="${pin.titulo}" style="cursor: pointer;" onclick="location.href='../previsualizacion/previsualizacion.html?id=${pin.id}'">
                 <div class="pin-info">
                     <h4>${pin.titulo}</h4>
                     <p>${pin.descripcion || 'Sin descripción.'}</p>
                 </div>
-            `;
-            grid.appendChild(card);
+            `);
+            
+            $grid.append($card);
         });
-    } catch (e) { console.error("Error cargando galería del perfil", e); }
+
+    } catch (error) {
+        console.error("Error cargando galería del perfil", error);
+    }
 }
